b4151c84f6fddf65c24bf0248739818b2f9e7a37
NATHAN JOHN CORDEIRO <155100838+NathanCordeiro@users.noreply.github.com>
2024-05-17 00:11:24 +0530

    Add files via upload

---
+++ EXP7/P1.py
@@ -0,0 +1,15 @@
+# Conditional probabilities
+# Nathan Cordeiro 22co09
+import matplotlib.pyplot as plt
+rain_prob = 0.3
+umbrella_given_rain_prob = 0.8
+umbrella_given_no_rain_prob = 0.2
+# Data for the bar chart
+x = ['Rain', 'No Rain']
+y = [umbrella_given_rain_prob, umbrella_given_no_rain_prob]
+# Plotting the bar chart
+plt.bar(x, y)
+plt.xlabel('Weather')
+plt.ylabel('Probability of Using Umbrella')
+plt.title('Conditional Probability: Use of Umbrella given Weather')
+plt.show()
